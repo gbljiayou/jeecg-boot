e118972c5142fdc64a9af9cf94e5dfef70c5cb35
gebaolu <gebaolu@gome.com.cn>
2019-11-20 16:23:57 +0800

    修改前端默认配置

--- ant-design-vue-jeecg/src/components/setting/SettingDrawer.vue
+++ ant-design-vue-jeecg/src/components/setting/SettingDrawer.vue
@@ -19,7 +19,8 @@
                 暗色菜单风格
               </template>
               <div class="setting-drawer-index-item" @click="handleMenuTheme('dark')">
-                <img src="https://gw.alipayobjects.com/zos/rmsportal/LCkqqYNmvBEbokSDscrm.svg" alt="dark">
+                <!-- <img src="https://gw.alipayobjects.com/zos/rmsportal/LCkqqYNmvBEbokSDscrm.svg" alt="dark"> -->
+								<img src="../../assets/dark-style.svg" alt="dark">
                 <div class="setting-drawer-index-selectIcon" v-if="navTheme === 'dark'">
                   <a-icon type="check"/>
                 </div>
@@ -31,7 +32,8 @@
                 亮色菜单风格
               </template>
               <div class="setting-drawer-index-item" @click="handleMenuTheme('light')">
-                <img src="https://gw.alipayobjects.com/zos/rmsportal/jpRkZQMyYRryryPNtyIC.svg" alt="light">
+                <!-- <img src="https://gw.alipayobjects.com/zos/rmsportal/jpRkZQMyYRryryPNtyIC.svg" alt="light"> -->
+								<img src="../../assets/dark-style.svg" alt="light">
                 <div class="setting-drawer-index-selectIcon" v-if="navTheme !== 'dark'">
                   <a-icon type="check"/>
                 </div>
--- ant-design-vue-jeecg/src/defaultSettings.js
+++ ant-design-vue-jeecg/src/defaultSettings.js
@@ -15,11 +15,11 @@
 
 export default {
   primaryColor: '#1890FF', // primary color of ant design
-  navTheme: 'light', // theme for nav menu
+  navTheme: 'dark', // theme for nav menu
   layout: 'sidemenu', // nav menu position: sidemenu or topmenu
   contentWidth: 'Fixed', // layout of content: Fluid or Fixed, only works when layout is topmenu
-  fixedHeader: false, // sticky header
-  fixSiderbar: false, // sticky siderbar
+  fixedHeader: true, // sticky header
+  fixSiderbar: true, // sticky siderbar
   autoHideHeader: false, //  auto hide header
   colorWeak: false,
   multipage: false, //默认多页签模式
